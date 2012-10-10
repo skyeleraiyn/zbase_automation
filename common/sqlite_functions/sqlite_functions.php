cdf5aba5a27b5a71c7825af39fcde54378c89944
Sriharsha BK <sbkrishnamurthy@zynga.com>
2012-10-10 18:13:42 +0530

    fixed unserialize word

--- common/sqlite_functions/sqlite_functions.php
+++ common/sqlite_functions/sqlite_functions.php
@@ -62,7 +62,7 @@ class sqlite_functions {
 	public function db_sqlite_select($remote_machine_name, $field, $table_name)	{
 		$db_checksum_array = array();
 		$temp_array = array();
-		foreach(unserialze(MEMBASE_DATABASE_PATH) as $membase_dbpath){
+		foreach(unserialize(MEMBASE_DATABASE_PATH) as $membase_dbpath){
 			for ($i=0; $i<4; $i++)	{
 				$db_checksum_array = array_merge($temp_array, explode("\n", self::sqlite_select($remote_machine_name, $field, $table_name, $membase_dbpath."/ep.db-$i.sqlite")));
 				$temp_array = $db_checksum_array;
