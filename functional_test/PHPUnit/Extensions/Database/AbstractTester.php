057205fe91d02eb2636330e97f8facacf0abd7d9
Aniraj Kesavan <akalathel@zynga.com>
2013-10-11 16:54:10 +0530

    Adding license

--- functional_test/PHPUnit/Extensions/Database/AbstractTester.php
+++ functional_test/PHPUnit/Extensions/Database/AbstractTester.php
@@ -1,7 +1,9 @@
 <?php
 /**
  * PHPUnit
- *
+ *Copyright 2013 Zynga Inc 
+ * Copyright 2013 Zynga Inc
+ * Copyright 2013 Zynga Inc 
  * Copyright (c) 2002-2010, Sebastian Bergmann <sb@sebastian-bergmann.de>.
  * All rights reserved.
  *
